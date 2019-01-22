9c49d46e5f4bf4136cba9f36e6abb6e4a22a2ed9
ddot727 <ddot727@gmail.com>
2019-01-22 18:26:38 -0500

    completed closure questions

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,26 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
+function howAreYou() {
+  let str = 'How are you?';
 
+  function howAreYouAlert () {
+    alert(str);
+  }
+  howAreYouAlert();
+}
+howAreYou();
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
+  let count = 0;
+  return () => (++count);
+}
   // Return a function that when invoked increments and returns a counter variable.
-};
+
+const newCounter = counter();
+console.log(newCounter());
+console.log(newCounter());
+console.log(newCounter());
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
